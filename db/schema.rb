cc00b0ae2bda6d00db0f28fb24d1778e05e8f7b2
natsumi <natsu.bsk810@gmail.com>
2024-08-23 08:59:44 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2024_08_22_084143) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
